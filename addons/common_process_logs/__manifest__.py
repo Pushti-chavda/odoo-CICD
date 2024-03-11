39da611cdd5596619f56f01844864885578f92cf
Pushti <127646512+Pushti-chavda@users.noreply.github.com>
2024-03-11 18:34:32 +0530

    Update __manifest__.py

--- addons/common_process_logs/__manifest__.py
+++ addons/common_process_logs/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
-    'name': 'Common Process Logs',
+    'name': 'Common Process',
     'version': '15.0.0.1',
     'category': 'Tools',
     'sequence': 11,
